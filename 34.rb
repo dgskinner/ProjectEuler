e37856b37b6e89e7339aaec69d0d3de866bfee6b
Duncan Skinner <skinnerduncan@gmail.com>
2015-04-16 12:06:58 -0700

    speed up using cache

--- 34.rb
+++ 34.rb
@@ -1,8 +1,15 @@
 # The trickiest part here is knowing where the upper bound is for numbers we need to check. I chose 2540160 because this is equal to 7 * factorial(9). For any number, we know that the sum of the factorials of the digits can't be greater than factorial(9) * (number_of_digits). We can be sure that for any number n greater than 7 * factorial(9), n > sum_of_digit_factorials(n) because at this point, the sum of the factorials of the digits is growing slower than the number itself.
 
+$factorial_cache = {}
+
 def factorial(n)
-  return 1 if n < 2
-  n * factorial(n - 1)
+  if $factorial_cache.has_key?(n)
+    return $factorial_cache[n]
+  else
+    result = n < 2 ? 1 : n * factorial(n - 1)
+    $factorial_cache[n] = result
+    return result
+  end
 end
 
 def sum_of_digit_factorials(num)
@@ -22,7 +29,6 @@ def digit_factorials
   total_sum
 end
 
-puts Time.now
 puts digit_factorials # => 40730
-puts Time.now
+
 
