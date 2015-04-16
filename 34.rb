a1816d3ba3767e750c284a4693899b6268ff7dd8
Duncan Skinner <skinnerduncan@gmail.com>
2015-04-16 11:44:10 -0700

    explain upper bound

--- 34.rb
+++ 34.rb
@@ -1,3 +1,5 @@
+# The trickiest part here is knowing where the upper bound is for numbers we need to check. I chose 2540160 because this is equal to 7 * factorial(9). For any number, we know that the sum of the factorials of the digits can't be greater than factorial(9) * (number_of_digits). We can be sure that for any number n greater than 7 * factorial(9), n > sum_of_digit_factorials(n) because at this point, the sum of the factorials of the digits is growing slower than the number itself.
+
 def factorial(n)
   return 1 if n < 2
   n * factorial(n - 1)
