d19a19f8d0ef967d645c43cc4fe998d9c9f6d830
Duncan Skinner <skinnerduncan@gmail.com>
2015-04-16 11:40:21 -0700

    refactor

--- 34.rb
+++ 34.rb
@@ -1,21 +1,26 @@
-def digit_factorials
-  total = 0
-  (10..2540160).each do |num|
-    num_str = num.to_s
-    sum = 0
-    num_str.each_char do |digit|
-      sum += factorial(digit.to_i)
-    end
-    total += num if num == sum
-  end
-  total
-end
-
 def factorial(n)
   return 1 if n < 2
   n * factorial(n - 1)
 end
 
+def sum_of_digit_factorials(num)
+  num_str = num.to_s
+  sum = 0
+  num_str.each_char do |digit|
+    sum += factorial(digit.to_i)
+  end
+  sum
+end
+
+def digit_factorials
+  total_sum = 0
+  (10..2540160).each do |num|
+    total_sum += num if num == sum_of_digit_factorials(num)
+  end
+  total_sum
+end
+
 puts Time.now
 puts digit_factorials # => 40730
 puts Time.now
+
